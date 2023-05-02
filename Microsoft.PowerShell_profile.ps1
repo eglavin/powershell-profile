4cba656252b2ea86f1009897b3fd0e76e2f32f63
Eanna Glavin <29385958+eglavin@users.noreply.github.com>
2023-05-02 14:48:08 +0100

    set ps read line options

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -12,19 +12,43 @@ Import-Module -Name Posh-Git
 
 # https://learn.microsoft.com/en-us/powershell/module/psreadline/set-psreadlineoption
 $PSReadLineOptions = @{
-  EditMode                      = "Windows"
-  PredictionSource              = "History"
+  PredictionSource              = "HistoryAndPlugin"
   PredictionViewStyle           = "ListView"
   HistoryNoDuplicates           = $true
   HistorySearchCursorMovesToEnd = $true
-  MaximumHistoryCount           = 5
 }
 Set-PSReadLineOption @PSReadLineOptions
 
-# https://github.com/dahlbyk/posh-git
-$env:POSH_GIT_ENABLED = $true
+# https://github.com/PowerShell/PSReadLine#usage
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineKeyHandler -Key Tab -Function Complete
 
-# https://ohmyposh.dev/
+Set-PSReadLineKeyHandler -Chord '"', "'" `
+  -BriefDescription SmartInsertQuote `
+  -LongDescription "Insert paired quotes if not already on a quote" `
+  -ScriptBlock {
+  param($key, $arg)
+
+  $line = $null
+  $cursor = $null
+  [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+
+  if ($line.Length -gt $cursor -and $line[$cursor] -eq $key.KeyChar) {
+    # Just move the cursor
+    [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
+  }
+  else {
+    # Insert matching quotes, move cursor to be in between the quotes
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$($key.KeyChar)" * 2)
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+    [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor - 1)
+  }
+}
+
+
+# https://ohmyposh.dev
+$env:POSH_GIT_ENABLED = $true
 oh-my-posh init pwsh --config "$PROFILE_DIR/hotstick.minimal.omp.json" | Invoke-Expression
 
 
